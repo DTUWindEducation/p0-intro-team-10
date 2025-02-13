bf5445d164fd7e1773e7b5f336b4d2ba4bf86de0
theodorbg <tgilhoj@gmail.com>
2025-02-13 16:25:14 +0100

    Deleted preclass_assignment folder

--- preclass_assignment/4.py
+++
@@ -1,7 +0,0 @@
-def fibonacci_stop(max_value):
-    fib_seq = [1,1]
-    while fib_seq[-1]+fib_seq[-2] <=max_value:
-        fib_seq.append(fib_seq[-1]+fib_seq[-2])
-    print(fib_seq)
-
-fibonacci_stop(30)
